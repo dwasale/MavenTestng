aa038c77196c3c09ba453389ed50b0fdbe7d1443
Rudra <dwasale@gmail.com>
2022-11-07 20:07:59 +0000

    Added new @Test method name toGetAllTheTextFromHomePageMenu()

--- src/main/java/com/rudra/MainMaven.java
+++ src/main/java/com/rudra/MainMaven.java
@@ -108,4 +108,20 @@ public class MainMaven {
         clickButton(By.name("send-email")); //It will click the send email button
         System.out.println(getText(By.xpath("//div[@class='message-error validation-summary-errors']"))); // It will print the given message either successful/unsuccessful
     }
+
+    //This method will get all the text from the HomePage
+    @Test
+    public static void toGetAllTheTextFromHomePageMenu(){
+        // Below statements from line 12-18 is just to show that given unique locator is navigation through the website as intended
+        clickButton(By.linkText("Computers"));
+        clickButton(By.linkText("Electronics"));
+        clickButton(By.linkText("Apparel"));
+        clickButton(By.linkText("Digital downloads"));
+        clickButton(By.linkText("Books"));
+        clickButton(By.linkText("Jewelry"));
+        clickButton(By.linkText("Gift Cards"));
+
+        // Prints all the String value of given unique locator in console
+        System.out.println(getText(By.className("header-menu")));
+    }
 }
